1c94e953656ea46f8608617f90ecdb85134d85aa
danielkang1003 <danielkang1003@gmail.com>
2019-11-03 13:51:42 +0900

    Create Customer DB Table & Node.js DB Conn Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 
 const bodyParser = require("body-parser");
@@ -8,33 +9,24 @@ const port = process.env.Port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json"); //json 읽어오기
+const conf = JSON.parse(data); //환경 설정 파싱해서 가져오기
+const mysql = require("mysql"); //mysql 불러오기
+
+const connection = mysql.createConnection({
+     host: conf.host,
+     user: conf.user,
+     password: conf.password,
+     port: conf.port,
+     database: conf.database
+});
+
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-     res.send([
-          {
-               id: 1,
-               image: "https://placeimg.com/128/128/any",
-               name: "떠돌이 방황자",
-               birth: "921003",
-               gender: "남자",
-               job: "피리부는 거지"
-          },
-          {
-               id: 2,
-               image: "https://placeimg.com/128/128/1",
-               name: "떠돌이 날치",
-               birth: "940203",
-               gender: "여자",
-               job: "뱃사공"
-          },
-          {
-               id: 3,
-               image: "https://placeimg.com/128/128/2",
-               name: "떠돌이 곰",
-               birth: "930727",
-               gender: "남자",
-               job: "제빵사"
-          }
-     ]);
+     connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+          res.send(rows);
+     });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
